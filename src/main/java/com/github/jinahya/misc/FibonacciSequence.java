401da3d62998490e7f7f5f9133609c04cad6f9fb
Jin Kwon <onacit@gmail.com>
2024-07-22 18:16:21 +0900

    wip

--- src/main/java/com/github/jinahya/misc/FibonacciSequence.java
+++ src/main/java/com/github/jinahya/misc/FibonacciSequence.java
@@ -1,5 +1,8 @@
 package com.github.jinahya.misc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.math.BigInteger;
 
 /**
@@ -22,24 +25,47 @@ import java.math.BigInteger;
  * </math>
  *
  * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ * @see <a href="https://r-knott.surrey.ac.uk/Fibonacci/fibtable.html">The first 300 Fibonacci numbers, completely
+ * factorised</a>
  */
 public final class FibonacciSequence {
 
-    public static void main(String... args) {
+    static final int MAX_N = 47; // 2971215073
 
-        final double d = 1.394232245617E+104;
-        System.out.println(d);
-        System.out.println(d > Long.MAX_VALUE);
-    }
+    static final int MAX_N_LONG = 87; // 679891637638612258
 
-    public static BigInteger getFibonacciNumber(final long n) {
+    static final int MAX_N_BIG = 127; // 155576970220531065681649693
+
+    private static int requirePositiveOrZero(final int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n(" + n + ") is negative");
+        }
+        return n;
+    }
+
+    public static @PositiveOrZero long getFibonacciNumber(final @Max(MAX_N) @PositiveOrZero int n) {
+        if (requirePositiveOrZero(n) > MAX_N) {
+            throw new IllegalArgumentException("n(" + n + ") is greater than " + MAX_N);
         }
         // TODO: implement!
         throw new UnsupportedOperationException("not implemented yet");
     }
 
+    public static @PositiveOrZero long getFibonacciNumberLong(final @Max(MAX_N_LONG) @PositiveOrZero int n) {
+        if (requirePositiveOrZero(n) > MAX_N_LONG) {
+            throw new IllegalArgumentException("n(" + n + ") is greater than " + MAX_N_LONG);
+        }
+        // TODO: implement!
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    public static BigInteger getFibonacciNumberBig(final @PositiveOrZero int n) {
+        requirePositiveOrZero(n);
+        // TODO: implement!
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
     private FibonacciSequence() {
         throw new AssertionError("instantiation is not allowed");
     }
